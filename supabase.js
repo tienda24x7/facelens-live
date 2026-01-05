15692ab9395ab9b642ac31de8a348b9804b0f736
Pablo Anania <pablo.anania@hotmail.com>
2026-01-05 20:52:21 -0300

    Fix global db

--- supabase.js
+++ supabase.js
@@ -2,15 +2,16 @@
 // Supabase client (GLOBAL)
 // =========================
 
-// URL de tu proyecto Supabase
 const SUPABASE_URL = "https://romgbrobvperljaviekj.supabase.co";
 
-// Anon public key (JWT largo que empieza con eyJ...)
 const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvbWdicm9idnBlcmxqYXZpZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3MTQ1MTMsImV4cCI6MjA4MjI5MDUxM30.WIpN_Hj0C70bD3Q-Bnxk4tfdgJRr8NnQwmJdM3C5HuI";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvbWdicm9idnBlcmxqYXZpZWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3MTQ1MTMsImV4cCI6MjA4MjI5MDUxM30.WIpN_Hj0C70bD3Q-Bnxk4tfdgJRr8NnQwmJdM3C5HuI";
 
-// Creamos el cliente y lo exponemos globalmente
-window.db = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// Crear cliente
+const supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Exponerlo globalmente de 2 formas:
+window.db = supa;
+
+// ✅ ESTO ES LO QUE TE FALTA (para que exista "db" en el HTML)
+var db = supa;
